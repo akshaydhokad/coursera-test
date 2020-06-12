6f494ffe62725d3f97a76fc8a34f3db13daf517f
akshaydhokad <64769982+akshaydhokad@users.noreply.github.com>
2020-06-12 17:32:11 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,1 +1,8 @@
-
+(function(window){
+	var helloSpeaker ={};
+	var speakWord = "Hello";
+	helloSpeaker.speak= function speak(name) {
+		 console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker=helloSpeaker;
+})(window);
